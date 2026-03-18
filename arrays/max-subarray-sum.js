6cdd2baaf213be023ffc006c1d84b688d938a803
jogisaikumar <jsaikumar8977@gmail.com>
2026-03-19 00:46:41 +0530

    Finished max sub array , beginner level pending on circular array and kadane + indices

---
+++ arrays/max-subarray-sum.js
@@ -0,0 +1,58 @@
+/* you are given an array of integers(positive + negative)
+    Find the contiguous subarray that return the maximum sum.
+    Input = [-2,1,-3,4-1,2,1,-5,4]
+
+*/
+
+// Let us see the brute force approach where the time complexity is O(n2)
+
+function maxSubArrSum(arr){
+    let maxSum = -Infinity;
+    for(let i=0;i<arr.length;i++){
+        let sum = 0;
+        for(let j=i;j<arr.length;j++){
+            sum += arr[j];
+            maxSum = Math.max(sum, maxSum);
+        }
+    }
+    return maxSum;
+}
+console.log(maxSubArrSum([-2,1,-3,4,-1,2,1,-5,4]));
+
+// Now let's proceed the with the optimal solution where time complexity is O(n) and space 
+// complexity is O(1) -------> Kadane's Algorithm;
+
+
+function maxSubArray(arr){
+    let currentSum = arr[0];
+    let maxSum = arr[0];
+    for(let i=1;i<arr.length;i++){
+        currentSum = Math.max(arr[i], currentSum + arr[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+    return maxSum;
+}
+
+const givenArr = [-2,1,-3,4,-1,2,1,-5,4];
+console.log(maxSubArray(givenArr));
+
+// return the max sum subarray from the given array using brute force technique;
+
+function subArray(arr){
+    let maxSum = -Infinity;
+    let start = 0;
+    let end = 0;
+    for(let i=0;i<arr.length;i++){
+        let sum = 0;
+        for(let j=i;j<arr.length;j++){
+            sum += arr[j];
+            if(sum > maxSum){
+                maxSum = sum;
+                start = i;
+                end = j;
+            }
+        }
+    }
+    return arr.slice(start, end+1);
+}
+console.log(subArray(givenArr));
